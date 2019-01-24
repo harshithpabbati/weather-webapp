a047a47a5093f775786fdf55230716071ebeaf57
Harshithpabbati <pabbatiharshitth@gmail.com>
2019-01-24 21:45:12 +0530

    a basic app with nodejs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Weather-app
+## It is a basic app which shows the weather of the particular place.
+## A app which uses the data from openweather website 
+### Made this app using nodejs.
+### How to run it in your local??
+>sudo-apt get update
+>git clone https://github.com/Harshithpabbati/Weather-app.git
+#### Now you need to install the dependicies
+>npm install
+#### Time to run it locally
+>node index.js
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+const apiKey = '****************************';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
